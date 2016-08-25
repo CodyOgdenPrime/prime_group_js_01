b5fcaba49d7c89ad29f3567060c560b0b44b23f0
Cody Ogden <cody@codyogden.com>
2016-08-25 16:06:31 -0500

    Functions complete

--- group_01.js
+++ group_01.js
@@ -4,3 +4,50 @@ var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
+
+
+// Function to redo employee arrays 
+
+
+// Function for switch -Jazzy
+var getRate = function ( rating ){
+	switch ( rating ) {
+		case 2: 
+			return 0;
+			break; 
+		case 3:
+			return 0.04;
+			break;
+		case 4:
+			return 0.06;
+			break;
+		case 5:
+			return 0.10;
+			break;
+		default: 
+			return false;
+	}
+}
+
+// Function (master)- Johanna
+
+var calculateBonus = function (emp){
+		var bonusRate = getRate();
+		if(bonusRate){
+			if (emp[1] >=4){
+				bonusRate += 0.05;
+			}
+			if (emp[2] > 65000){
+				bonusRate -= 0.01;
+			}
+
+			if (emp[3] > 0.13){
+				bonusRate = 0.13;
+			}	
+		}
+		return bonusRate;
+}	
+
+
+// theLoop - Justin O'Connor
+
