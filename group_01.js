3a2092400ed75547d470f6675ffcae782a946680
Cody Ogden <cody@codyogden.com>
2016-08-25 16:37:14 -0500

    Everything is working

--- group_01.js
+++ group_01.js
@@ -1,3 +1,4 @@
+
 var atticus = ["Atticus", "2405", "47000", 3];
 var jem = ["Jem", "62347", "63500", 4];
 var boo = ["Boo", "11435", "54000", 3];
@@ -6,11 +7,10 @@ var scout = ["Scout", "6243", "74750", 5];
 var employees = [atticus, jem, boo, scout];
 
 
-// Function to redo employee arrays 
-
-
 // Function for switch -Jazzy
 var getRate = function ( rating ){
+
+	// Switch through employee ratiings
 	switch ( rating ) {
 		case 2: 
 			return 0;
@@ -31,23 +31,45 @@ var getRate = function ( rating ){
 
 // Function (master)- Johanna
 
-var calculateBonus = function (emp){
-		var bonusRate = getRate();
+var calculateBonus = function ( emp ){
+		// Get the bonus rate based on the employee rating
+		var bonusRate = getRate( emp[3] );
+        // console.log( bonusRate );
+		// Bonus rate checks
 		if(bonusRate){
-			if (emp[1] >=4){
+			// If the rating is >= 4, add 0.05 to bonus rate.
+          // console.log( "employee number:",  emp[1]);
+			if ( emp[1].length >= 4){
 				bonusRate += 0.05;
 			}
+          // console.log( "Rating >= 4?",bonusRate );
+			// if salary is greater than 650000
 			if (emp[2] > 65000){
 				bonusRate -= 0.01;
 			}
-
-			if (emp[3] > 0.13){
+			// If rate is > 13%, max is 13%, set it to 13%
+			if ( bonusRate > 0.13){
 				bonusRate = 0.13;
 			}	
 		}
+		// Send that rate packing
 		return bonusRate;
-}	
+}
 
 
-// theLoop - Justin O'Connor
+// Function to redo employee arrays - Cody & Justin
+// for each of the employees array
+for ( var i = 0; i < employees.length; i++ ) {
+	var newEmployeeArr = [];
+	var oldEmployeeArr = employees[i];
+	var bonusRate = calculateBonus( oldEmployeeArr );
+	var newSalary = parseInt( oldEmployeeArr[2] ) + ( oldEmployeeArr[2] * bonusRate );
 
+	newEmployeeArr[0] = oldEmployeeArr[0];
+	newEmployeeArr[1] = bonusRate;
+	newEmployeeArr[2] = newSalary;
+	newEmployeeArr[3] = Math.round( newSalary );
+
+	console.log( newEmployeeArr );
+
+}
