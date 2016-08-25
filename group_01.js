aeb1bb98b0c333fdc3ab379dc05ec7146b2eb6b8
Cody Ogden <cody@codyogden.com>
2016-08-25 16:54:39 -0500

    Add comments - justin

--- group_01.js
+++ group_01.js
@@ -1,4 +1,5 @@
 
+//global variables/arrays
 var atticus = ["Atticus", "2405", "47000", 3];
 var jem = ["Jem", "62347", "63500", 4];
 var boo = ["Boo", "11435", "54000", 3];
@@ -10,17 +11,21 @@ var employees = [atticus, jem, boo, scout];
 // Function for switch -Jazzy
 var getRate = function ( rating ){
 
-	// Switch through employee ratiings
+	// Switch through employees to get correct bonus rate
 	switch ( rating ) {
+		// if rating equals 2, no bonus
 		case 2: 
 			return 0;
 			break; 
+		// if rating equals 3, 4% bonus
 		case 3:
 			return 0.04;
 			break;
+		// if rating equals 4, 6% bonus
 		case 4:
 			return 0.06;
 			break;
+		// if rating equals 5, 10% bonus
 		case 5:
 			return 0.10;
 			break;
@@ -30,19 +35,16 @@ var getRate = function ( rating ){
 }
 
 // Function (master)- Johanna
-
+//function to apply the bonus rate found in the switch above named getRate
 var calculateBonus = function ( emp ){
 		// Get the bonus rate based on the employee rating
 		var bonusRate = getRate( emp[3] );
-        // console.log( bonusRate );
 		// Bonus rate checks
 		if(bonusRate){
 			// If the rating is >= 4, add 0.05 to bonus rate.
-          // console.log( "employee number:",  emp[1]);
 			if ( emp[1].length >= 4){
 				bonusRate += 0.05;
 			}
-          // console.log( "Rating >= 4?",bonusRate );
 			// if salary is greater than 650000
 			if (emp[2] > 65000){
 				bonusRate -= 0.01;
@@ -62,9 +64,12 @@ var calculateBonus = function ( emp ){
 for ( var i = 0; i < employees.length; i++ ) {
 	var newEmployeeArr = [];
 	var oldEmployeeArr = employees[i];
+	// get the bonus % from calculateBonus
 	var bonusRate = calculateBonus( oldEmployeeArr );
+	// multiply original salary by bonus rate, then add to bonus to get total of salary + bonus
 	var newSalary = parseInt( oldEmployeeArr[2] ) + ( oldEmployeeArr[2] * bonusRate );
 
+	// create new array in order: Employee Name, calculated bonus rate, new salary, rounded new salary
 	newEmployeeArr[0] = oldEmployeeArr[0];
 	newEmployeeArr[1] = bonusRate;
 	newEmployeeArr[2] = newSalary;
